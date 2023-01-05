a9d2644d141ea48a7c4ba3122b7638a4c1b38059
Niel-Reitmann <nielreitmann@gmail.com>
2023-01-05 13:36:35 +1000

    test3

--- copy.py
+++ copy.py
@@ -1,3 +1,18 @@
-print ('hello')
+import os
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Say this is a test",
+  max_tokens=7,
+  temperature=0
+)
 
-#this is a test to see if it is now only one branch
+print (response)
+
+#PARAMETERS;
+
+#model, which model is being used
+#prompt is a string or array of strings that will be used to generate completion
+#max tokens refers to the length of completion
+#temperature refers to how random the response will be, between 0 and 0.9
